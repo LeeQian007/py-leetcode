2dcdaff1606c37b3e3c8353a912904734f3d496f
LiQiang <131451096+LeeQian007@users.noreply.github.com>
2024-03-01 23:16:40 +0800

    Mar 1

---
+++ 215.Kth-largest-element-in-a-array.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        # heap: n + klog(n)  become a heap + pop "k" heap
+        # Quick Select
+        # the index of target when array was sorted
+        k = len(nums) - k
+
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l , r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k:   return quickSelect(l, p - 1)
+            elif p < k: return quickSelect(p + 1, r)
+            else:       return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
