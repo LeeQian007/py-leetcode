ed876b1c66956c6a32e57ef18932c10a52133148
LiQiang <131451096+LeeQian007@users.noreply.github.com>
2024-03-03 15:38:24 +0800

    Mar 1

---
+++ 114.flatten-Tree.py
@@ -0,0 +1,25 @@
+from idlelib.tree import TreeNode
+from typing import Optional
+
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+        def dfs(root):
+            if not root:
+                return None
+
+            leftTail = dfs(root.left)
+            rightTail = dfs(root.right)
+
+            if root.left:
+                leftTail.right = root.right
+                root.right = root.left
+                root.left = None
+            return (rightTail or leftTail or root)
+
+        dfs(root)
+            
