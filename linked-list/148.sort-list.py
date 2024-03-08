35c013b80ea14156600bbc550564edf9d1e0845c
LiQiang <131451096+LeeQian007@users.noreply.github.com>
2024-03-08 14:51:29 +0800

    Mar 8

---
+++ linked-list/148.sort-list.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        # split the list into two halves
+        left = head
+        right = self.getMid(head)
+        temp = right.next
+        right.next = None
+        right = temp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        self.merge(left, right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, left, right):
+        dummy = tail = ListNode()
+
+        while left and right:
+            if left.val > right.val:
+                tail.next = right
+                right = right.next
+            else:
+                tail.next = left
+                left = left.next
+            tail = tail.next
+        if left:
+            tail.next = left
+        if right:
+            tail.next = right
+        return dummy.next
