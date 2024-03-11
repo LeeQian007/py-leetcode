371bbdfb6ad2070967acff9f26c39d1fdb63af2d
LiQiang <131451096+LeeQian007@users.noreply.github.com>
2024-03-11 15:31:10 +0800

    Mar 11

---
+++ dfs/437.path-sum-3.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        def rootSum(root, curSum):
+            if root is None:
+                return 0
+            ret = 0
+            curSum += root.val
+            if curSum == targetSum:
+                ret += 1
+            ret += rootSum(root.left, curSum)
+            ret += rootSum(root.right, curSum)
+            return ret
+
+        ret = rootSum(root, 0)
+        ret += self.pathSum(root.left, targetSum)
+        ret += self.pathSum(root.right, targetSum)
+
+        return ret
